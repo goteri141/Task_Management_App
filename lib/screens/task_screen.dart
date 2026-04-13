6ddc03e99fddbef19b94931b8a5808bafcd6182a
Gloria Oteri <gloriaoteri@gmail.com>
2026-04-12 21:49:51 -0400

    Deleted firestore version of addTask, deleteTask, and isComplete toggle

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/task.dart';
+
 
 class TaskListScreen extends StatefulWidget {
   const TaskListScreen({super.key});
@@ -26,34 +26,6 @@ class _TaskListScreenState extends State<TaskListScreen> {
       ));
     });
   }
-
-// -- Firestore version -----------------
-Future<void> addTaskToFirestore(String title) async {
-  if (title.trim().isEmpty) return; // Same validation rule
-
-  await FirebaseFirestore.instance.collection('tasks').add({
-    'title': title.trim(),
-    'isCompleted': false,
-    'subtasks': [],
-    'createdAt': DateTime.now().toIso8601String()
-  }); // No setState() needed here - the stream will push the new doc
-}
-
-// Toggle isCompleted in Firestore
-Future<void> toggleTask(Task task) async {
-    await FirebaseFirestore.instance
-      .collection('tasks')
-      .doc(task.id)
-      .update({'isCompleted': !task.isCompleted});
-}
-
-// Permanently delete a task
-Future<void> deleteTask(String taskId) async {
-    await FirebaseFirestore.instance
-      .collection('tasks')
-      .doc(taskId)
-      .delete();
-}
 
   @override
   void dispose() {
